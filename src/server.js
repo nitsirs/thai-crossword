f2d39125c88082b5cac09a0fcd7420c5a4652d5d
Nithiwat Sirirattanachaikul <nitsirs@gmail.com>
2022-04-24 16:56:37 +0700

    add Procfile

--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
-import { Server } from 'boardgame.io/server';
-import path from 'path';
-import serve from 'koa-static';
-const { ThaiCrossword } = require('./game/game');
+import { Server, Origins } from "boardgame.io/server";
+import path from "path";
+import serve from "koa-static";
+const { ThaiCrossword } = require("./game/game");
 
-const server = Server({ games: [ThaiCrossword] });
+const server = Server({ games: [ThaiCrossword], origins: [Origins.LOCALHOST] });
 const PORT = process.env.PORT || 8000;
 
 // Build path relative to the server.js file
-const frontEndAppBuildPath = path.resolve(__dirname, './build');
-server.app.use(serve(frontEndAppBuildPath))
+const frontEndAppBuildPath = path.resolve(__dirname, "./build");
+server.app.use(serve(frontEndAppBuildPath));
 
 server.run(PORT, () => {
   server.app.use(
-    async (ctx, next) => await serve(frontEndAppBuildPath)(
-      Object.assign(ctx, { path: 'index.html' }),
-      next
-    )
-  )
+    async (ctx, next) =>
+      await serve(frontEndAppBuildPath)(
+        Object.assign(ctx, { path: "index.html" }),
+        next
+      )
+  );
 });
